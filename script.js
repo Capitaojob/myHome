5b33af9e4567da7c971e5d95104945ad2b088415
Joao Bataglia <jpangottibataglia@gmail.com>
2023-01-07 11:07:04 -0300

    feature: added cuckoo clock

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 "use strict"
 
+// DECLARATIONS OF CONSTANTS
 const root = document.querySelector(":root")
 const self = document.querySelector(".self")
 const star = document.querySelector(".fa-regular")
 const time = document.querySelector(".time")
 const day = document.querySelector(".day")
+const cuckoo = new Audio("cuckoo.mp3")
+cuckoo.volume = 0.2
 
-var data = new Date
-var hour = data.getHours()
-var minute = data.getMinutes()
-var second = data.getSeconds()
+// WAIT FUNCTION
+// function wait(ms){
+//     var start = new Date().getTime();
+//     var end = start;
+//     while(end < start + ms) {
+//       end = new Date().getTime();
+//    }
+// }
 
-function timing() {
-    data = new Date
-    hour = data.getHours()
-    minute = data.getMinutes()
-    second = data.getSeconds()
-    minute = checkTime(minute)
-    second = checkTime(second)
-    // time.innerHTML = `${hour}:${minute}:${second}`
-    time.innerHTML = hour + ":" + minute + ":" + second
-}
-
-timing()
-setInterval(timing, 1000);
-
-day.innerHTML = `0${data.getDate()}/0${data.getMonth()+1}/${data.getFullYear()}`
-
+// RANDOM PICTURE
 var random = ((Math.ceil((Math.random())*100))%3)+1
 
 if (random == 1) {
@@ -36,6 +28,55 @@ if (random == 1) {
 } else if (random == 3) {
     self.src="images/think.png.jpg"
 }
+
+// DECLARATIONS OF TIME VARIABLES
+var data = new Date
+var hour = data.getHours()
+var minute = data.getMinutes()
+var second = data.getSeconds()
+var lastSecond
+
+// CLOCK TIMER FUNCTION
+function timing() {
+    lastSecond = second
+
+    data = new Date
+    hour = data.getHours()
+    minute = data.getMinutes()
+    second = data.getSeconds()
+    minute = checkTime(minute)
+    second = checkTime(second)
+    // time.innerHTML = `${hour}:${minute}:${second}`
+    time.innerHTML = hour + ":" + minute + ":" + second
+
+    cuckooFunction()
+}
+
+// CUCKOO CLOCK FUNCTION
+
+async function cuckooFunction() {
+    if (minute == '00' && second == '00') {
+        cuckoo.play()
+        self.src="images/cuckoo-clock.gif"
+        // console.log("hell")
+        await new Promise(r => setTimeout(r, 10000));
+        // console.log("hello")
+        if (random == 1) {
+            self.src="images/blink.png.jpg"
+        } else if (random == 2) {
+            self.src="images/look.png.jpg"
+        } else if (random == 3) {
+            self.src="images/think.png.jpg"
+        }
+    }
+}
+
+//-----------------------------------
+
+timing()
+setInterval(timing, 999);
+
+// day.innerHTML = `0${data.getDate()}/0${data.getMonth()+1}/${data.getFullYear()}`
 
 if (hour >= 6 && hour < 12) {
     document.querySelector(".initalText").innerHTML = "Bom Dia, João!"
